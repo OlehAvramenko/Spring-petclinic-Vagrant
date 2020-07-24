f43afbb6ad09d9349f532e4cb16514dbfecdac8c
OlehAvramenko <68397087+OlehAvramenko@users.noreply.github.com>
2020-07-24 21:18:30 +0300

    Update everyboot.sh

--- everyboot.sh
+++ everyboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo -E -u $APP_USER java -jar  $APP_DIR/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql >> $APP_DIR/log &
-/vagrant/healthcheck.sh
+/vagrant/Healthcheck.sh
