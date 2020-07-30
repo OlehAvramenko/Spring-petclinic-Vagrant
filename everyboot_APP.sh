62be481784a9a292d7e41ed794eb1ffd34797705
OlehAvramenko <68397087+OlehAvramenko@users.noreply.github.com>
2020-07-30 20:35:22 +0300

    Update everyboot_APP.sh

--- everyboot_APP.sh
+++ everyboot_APP.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD:everyboot_APP.sh
 sudo -E -u $APP_USER java -jar  $APP_DIR/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql >> $APP_DIR/log_Mysql || sudo -E -u $APP_USER java -jar  $APP_DIR/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar >> $APP_DIR/log_H2 &
-=======
-sudo -E -u $APP_USER java -jar  $APP_DIR/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql >> $APP_DIR/log &
->>>>>>> ab8544e2c2786217fede06c9680f0f5dba4d6fc1:evryboot.sh
+# sudo -E -u $APP_USER java -jar  $APP_DIR/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql >> $APP_DIR/log &
 /vagrant/health.sh
