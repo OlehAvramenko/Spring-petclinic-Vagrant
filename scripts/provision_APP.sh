14a84d352cabb324f29a2a63dcbd643f9da99a84
OlehAvramenko <mail@yourmail.ru>
2020-08-14 15:15:19 +0300

    Add some commnets

--- scripts/provision_APP.sh
+++ scripts/provision_APP.sh
@@ -10,6 +10,7 @@ echo -e "$PASS\n$PASS\n" | sudo passwd $APP_USER
 apt -y update
 apt install -y openjdk-11-jdk
 cd /home/vagrant && git clone $URL
+# ================== BUILD APP ============================
 cp /vagrant/application-mysql.properties $PROJECT_DIR/src/main/resources/application-mysql.properties
 cd $PROJECT_DIR && ./mvnw test && ./mvnw package
 cp $PROJECT_DIR/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar  $APP_DIR
@@ -18,4 +19,4 @@ cp $PROJECT_DIR/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar  $APP_DIR
 
 # su -l $APP_USER -c 'java -jar  spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql'
 # sudo -E -u $APP_USER java -jar  $APP_DIR/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql >> $APP_DIR/log &
-# /vagrant/Healthcheck.sh
+# /vagrant/scripts/health.sh
